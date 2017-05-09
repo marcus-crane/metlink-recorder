472753cc8e96e1bbe5d566b70753cf1fcdf936ea
Marcus Crane <marcus@thingsima.de>
2017-05-09 22:17:41 +1200

    Works enough to be set as a cron job

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 const h = require('./helpers')
 
-h.fetchService('NAEN')
-.then((APIresponse) => {
-  insertOperatingServices(APIresponse)
-})
-.catch((err) => {
-  console.error('failed to fetch latest service', err)
-})
+h.getNAENupdate()
--- helpers.js
+++ helpers.js
@@ -1,10 +1,51 @@
 const r = require('rethinkdbdash')({ db: 'metlink' })
 const axios = require('axios')
 
-fetchService = (stationCode) => {
+fetchData = (stationCode) => {
   return axios.get(`https://www.metlink.org.nz/api/v1/StopDepartures/${stationCode}`)
   .then((res) => { return res.data })
   .catch((err) => { console.error('error fetching station data', err) })
+}
+
+queryTrip = (intendedDeparture) => {
+  return r.table('services')
+  .filter(r.row('intendedDeparture').eq(intendedDeparture))
+  .then(service => {
+    if (service.length) {
+      return service[0].id
+    } else {
+      return false
+    }
+  })
+  .catch(err => console.error(err))
+}
+
+upsertData = (service) => {
+  queryTrip(service.AimedDeparture)
+  .then(id => {
+    if (id) {
+      updateData(service, id)
+    } else {
+      insertData(service)
+    }
+  })
+}
+
+updateData = (service, id) => {
+  r.table('services')
+  .get(id)
+  .update({
+    'intendedArrival': service.AimedArrival,
+    'expectedDeparture': service.ExpectedDeparture,
+    'status': service.DepartureStatus,
+    'modified': r.now()
+  })
+  .then((res) => {
+    console.log('updated', res)
+  })
+  .catch((err) => {
+    console.log('error updating', err)
+  })
 }
 
 insertData = (service) => {
@@ -23,17 +64,45 @@ insertData = (service) => {
     'name': service.Service.Name,
     'createdAt': r.now()
   })
+  .then((res) => {
+    console.log('inserted', res)
+  })
   .catch((err) => {
-    console.error('failed to insert data', err)
+    console.error('error inserting', err)
   })
 }
 
 insertOperatingServices = (APIresponse) => {
   for (service of APIresponse.Services) {
     if (service.VehicleRef) {
-      insertData(service)
+      upsertData(service)
     }
   }
 }
 
-module.exports = { fetchService, insertOperatingServices }
+queryService = (stationCode) => {
+  return fetchData(stationCode)
+  .then((APIresponse) => {
+    insertOperatingServices(APIresponse)
+  })
+  .catch((err) => {
+    console.error('failed to fetch latest service', err)
+  })
+}
+
+drainPool = () => {
+  r.getPoolMaster().drain()
+}
+
+getNAENupdate = () => {
+  queryService('NAEN')
+  .catch((err) => {
+    console.log('failed to update NAEN data')
+  })
+
+  setTimeout(drainPool, 3000)
+}
+
+module.exports = {
+  getNAENupdate
+}
